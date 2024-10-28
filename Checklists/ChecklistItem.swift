020c9a1df5ff3a688c4b1742a62ec50dc1b95600
Jashan Kishore <jashan.kishore@gmail.com>
2024-10-29 10:56:12 +1100

    Initial commit - TableView setup with add new rows button and swipe to delete function

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Jashan kishore on 28/10/2024.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
---
+++ Checklists/ChecklistViewController.swift
@@ -0,0 +1,113 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Jashan kishore on 24/10/2024.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+    var items = [ChecklistItem]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let item1 = ChecklistItem()
+        item1.text = "Walk the dog"
+        items.append(item1)
+        
+        let item2 = ChecklistItem()
+        item2.text = "Brush my teeth"
+        item2.checked = true
+        items.append(item2)
+        
+        let item3 = ChecklistItem()
+        item3.text = "Learn iOS development"
+        item3.checked = true
+        items.append(item3)
+        
+        let item4 = ChecklistItem()
+        item4.text = "Soccer practice"
+        items.append(item4)
+        
+        let item5 = ChecklistItem()
+        item5.text = "Eat ice cream"
+        items.append(item5)
+        
+        let item6 = ChecklistItem()
+        item6.text = "Feed the cat"
+        items.append(item6)
+        
+        let item7 = ChecklistItem()
+        item7.text = "Clean the windows"
+        items.append(item7)
+    }
+    
+    
+    //MARK: - Actions
+    @IBAction func addItem(){
+        let newRowIndex = items.count
+        
+        let item = ChecklistItem()
+        item.text = "I am a new row"
+        item.checked = true
+        items.append(item)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+    }
+    
+    
+    // MARK: - Table View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        let item = items[indexPath.row]
+        configureCheckMark(for: cell, with: item)
+        configureText(for: cell, with: item)
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            let item = items[indexPath.row]
+            item.checked.toggle()
+            configureCheckMark(for: cell, with: item)
+            configureText(for: cell, with: item)
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+        
+        
+    func configureCheckMark(for cell: UITableViewCell, with item: ChecklistItem) {
+        if item.checked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+    }
+    
+    
+    func configureText(for cell: UITableViewCell, with item: ChecklistItem) {
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = item.text
+    }
+    
+    
+    // Delegate function to delete rows from model and view using swipe gesture
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        items.remove(at: indexPath.row)
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
+    
+    
+}
