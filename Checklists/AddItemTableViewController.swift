cd42f6dadc08bd9df65b2d1725d5a512ed12f96a
Jashan Kishore <jashan.kishore@gmail.com>
2024-10-29 15:07:51 +1100

    Implement text field delegate methods in addItemVC

--- Checklists/AddItemTableViewController.swift
+++ Checklists/AddItemTableViewController.swift
@@ -7,12 +7,23 @@
 
 import UIKit
 
-class AddItemTableViewController: UITableViewController {
-
+class AddItemTableViewController: UITableViewController, UITextFieldDelegate {
+    // MARK: - Outlets
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
     }
+    
+    
+    // Automatically show keyboard when visiting this screen
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
     
     // MARK: - Actions
     @IBAction func cancelTapped() {
@@ -20,6 +31,29 @@ class AddItemTableViewController: UITableViewController {
     }
     
     @IBAction func doneTapped() {
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
+    }
+    
+    
+    // MARK: - Table View Delegates
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    
+    // MARK: - Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+    }
+    
+    // Use the clear button while also disabling done button correctly
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
     }
 }
